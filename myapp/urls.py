957e914403ae156ea661443bcb8877d90626510e
IsiOgas <isidoraogaspavez@gmail.com>
2024-10-23 18:00:47 -0300

    creacion de pagina de inicio y botones (cambiar colores)

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('mapa/', views.mapa),
-    path('reportes/', views.reportes)
+    path('', views.home, name='home'),
+    path('mapa/', views.mapa, name='mapa'),
+    path('reportes/', views.reportes, name='reportes'),
 ]
